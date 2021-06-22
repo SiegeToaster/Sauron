bd8bf162176817170cd20ac5a9e229495d878eb5
Siege <81945479+SiegeToaster@users.noreply.github.com>
2021-06-21 18:10:39 -0700

    Change to SQL database

--- index.js
+++ index.js
@@ -2,16 +2,6 @@
 // =====SETUP=====\\
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require('fs');
-const readline = require('readline');
-const { google } = require('googleapis');
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
-const TOKEN_PATH = 'token.json';
-fs.readFile('credentials.json', (err, content) => {
-    if (err) return console.log('Error loading client secret file:', err);
-    authorize(JSON.parse(content));
-});
-let authCode = '';
 let reactedUsers = [`
 > ⠀⠀⠉⠛⠛⠻⢿⣿⣿⣿⣿⣿⣿⣶⣶⣶⣶⣶⣶⣶⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀
 > ⠀⠀⠀⠀⠀⠀⠚⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⠀⠀
@@ -29,6 +19,25 @@ let reactedUsers = [`
 > ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀
 > ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠺⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁
 `];
+
+const mysql = require('mysql');
+const { SQLPassword } = require('./private.json');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: SQLPassword,
+	database: 'sauronScores',
+});
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('Connected to SQL server.');
+	const sql = "INSERT INTO scores (user) VALUES (1)";
+	connection.query(sql, function(err, result) {
+    if (err) throw err;
+    console.log("table altered");
+	console.log(result);
+  });
+});
 
 client.once('ready', () => {
     console.log('');
@@ -99,16 +108,16 @@ client.on ('message', message => {
     switch (command) {
 	case 'ping':
         message.channel.send('Pong.');
-        break;
+    break;
 
     case 'help':
         message.channel.send(helpEmbed);
-        break;
+    break;
 
 	case 'catjam':
         message.delete();
         message.channel.send('https://tenor.com/view/cat-cat-jam-nod-pet-kitty-gif-17932554');
-        break;
+    break;
 
     case 'ben10':
         message.delete();
@@ -120,21 +129,21 @@ client.on ('message', message => {
                 message.channel.send(`${fullMessage}`);
             }
             message.channel.send('https://media.discordapp.net/attachments/831202194673107005/844378006147694622/Thats_far_enough.PNG');
-        break;
+    break;
 
     case 'delete':
         message.delete();
         message.channel.send('https://media.discordapp.net/attachments/761347053983891499/842548851310985236/delete.jpg');
         // ToDo: delete replied message if message is a reply.
-        break;
+    break;
 
     case 'joe':
         message.channel.send('joe mama');
-        break;
+    break;
 
     case 'ligma':
         message.channel.send('ligma balls');
-        break;
+    break;
 
 	case 'absent':
 		let offlineMembers = getOfflineMembers(message.mentions.members.array(), message);
@@ -153,7 +162,7 @@ client.on ('message', message => {
             }
             message.channel.send(`${offlineMembers} are absent <:FeelsBadMan:794744572718481408>`);
         }
-        break;
+    break;
 
     case 'troll':
         message.delete();
@@ -171,7 +180,7 @@ client.on ('message', message => {
                 message.channel.send(fullMessage);
             }
         }
-        break;
+    break;
 
     case 'gamble':
         // eslint-disable-next-line no-unused-vars
@@ -188,7 +197,7 @@ client.on ('message', message => {
                     }
                 });
         });
-        break;
+    break;
 
     case 'coinflip':
         const flipNumber = Math.round(Math.random());
@@ -198,7 +207,7 @@ client.on ('message', message => {
         } else {
             message.channel.send('Tails!');
         }
-        break;
+    break;
 
     case 'jamtime':
             reactedUsers = [];
@@ -228,183 +237,15 @@ client.on ('message', message => {
                         });
                     });
             });
-            break;
-
-        case 'rate':
-            if (args[0] == `<@!${message.author.id}>`) return message.channel.send('Invalid user - Rule 10 <:FeelsWeirdMan:792656734409195542>');
-            if (args[1] < 1 || args[1] > 10) return message.channel.send('Invalid rating <:FeelsWeirdMan:792656734409195542>');
-            switch (args[0]) {
-                case '<@!356642729394044932>':
-                    args[0] = 'B2:D2';
-                    break;
-
-                case '<@!306589457908498433>':
-                    args[0] = 'B3:D3';
-                    break;
-
-                case '<@!495290130924437516>':
-                    args[0] = 'B4:D4';
-                    break;
-            }
-            setScore(authCode, args[0], args[1], message);
-            break;
-
-        case 'getscore':
-            if (message.mentions.members.array().length < 1) {
-                getTotalScore(authCode, message);
-            } else {
-                message.mentions.members.array().forEach(element => {
-                    console.log(element.id);
-                    switch (element.id) {
-                        case '356642729394044932':
-                        element = 'A2:E2';
-                        break;
-
-                    case '306589457908498433':
-                        element = 'A3:E3';
-                        break;
-
-                    case '495290130924437516':
-                        element = 'A4:E4';
-                        break;
-                    }
-                    getIndividualScore(authCode, element, message);
-                });
-            }
-            break;
+        break;
 
         case 'test':
             message.channel.send('no tests today <:pepePOG:796983161249988648>');
-            break;
+        break;
 	}
 });
 
 // =====Functions===== \\
-function authorize(credentials) {
-    const { client_secret, client_id, redirect_uris } = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
-
-    fs.readFile(TOKEN_PATH, (err, token) => {
-        if (err) return getNewToken(oAuth2Client);
-        oAuth2Client.setCredentials(JSON.parse(token));
-        authCode = oAuth2Client;
-    });
-}
-
-function getNewToken(oAuth2Client) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: SCOPES,
-    });
-    console.log('Authorize this app by visiting this url: ', authUrl);
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-    rl.question('Enter the code from that page here: ', (code) => {
-        rl.close();
-        oAuth2Client.getToken(code, (err, token) => {
-            if (err) return console.error('Error while trying to retrieve access token', err);
-            oAuth2Client.setCredentials(token);
-            fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-                if (err) return console.error(err);
-                console.log('Token stored to ', TOKEN_PATH);
-            });
-        });
-    });
-}
-
-function getTotalScore(auth, message) {
-    const sheets = google.sheets({ version: 'v4', auth });
-    sheets.spreadsheets.values.get({
-        spreadsheetId: '1S0-MC0BWaGxhybXlpmE9Eu9ctsjeQ2Bjdha9DBnFFHo',
-        range: 'A2:E4',
-    }, (err, res) => {
-        if (err) {
-            message.channel.send('Failed to get scores <:FeelsDankMan:794744902172540968>');
-            console.log(`The API returned an error: ${err}`);
-        }
-        const rows = res.data.values;
-        if (rows.length && message) {
-            rows.map((row) => {
-                message.channel.send(`${(message.guild.members.cache.get(row[0])).nickname} - Total Score: ${row[1]}, Number of Ratings: ${row[2]}, Highest Rating: ${row[3]}, Average Rating: ${row[4]}`);
-            });
-        } else {
-            console.log('No data found.');
-        }
-    });
-}
-
-async function getSpecificScore(auth, range) {
-    const promise = new Promise((resolve) => {
-        const sheets = google.sheets({ version: 'v4', auth });
-        sheets.spreadsheets.values.get({
-            spreadsheetId: '1S0-MC0BWaGxhybXlpmE9Eu9ctsjeQ2Bjdha9DBnFFHo',
-            range: range,
-        }, (err, res) => {
-            if (err) {
-                resolve(false);
-                return console.log(`The API returned an error: ${err}`);
-            }
-            const rows = res.data.values;
-            resolve(rows);
-        });
-    });
-    return await promise;
-}
-
-function getIndividualScore(auth, range, message) {
-    const sheets = google.sheets({ version: 'v4', auth });
-    sheets.spreadsheets.values.get({
-        spreadsheetId: '1S0-MC0BWaGxhybXlpmE9Eu9ctsjeQ2Bjdha9DBnFFHo',
-        range: range,
-    }, (err, res) => {
-        if (err) {
-            message.channel.send('Failed to get scores <:FeelsDankMan:794744902172540968>');
-            return console.log(`The API returned an error: ${err}`);
-        }
-        const row = res.data.values[0];
-        if (row.length && message) {
-            message.channel.send(`${(message.guild.members.cache.get(row[0])).nickname} - Total Score: ${row[1]}, Number of Ratings: ${row[2]}, Highest Rating: ${row[3]}, Average Rating: ${row[4]}`);
-        } else {
-            console.log('No data found.');
-        }
-    });
-}
-
-async function setScore(auth, range, value, message) {
-    const previousValues = await getSpecificScore(authCode, range);
-    if (!previousValues) {
-        message.channel.send('Failed to add score.');
-    }
-    value = parseInt(value);
-    const previousScore = parseInt(previousValues[0][0]);
-    const previousNumber = parseInt(previousValues[0][1]);
-    let highest = parseInt(previousValues[0][2]);
-    if (value > highest) {
-        highest = value;
-    }
-    const sheets = google.sheets({ version: 'v4', auth });
-    const request = {
-        spreadsheetId: '1S0-MC0BWaGxhybXlpmE9Eu9ctsjeQ2Bjdha9DBnFFHo',
-        range: range,
-        valueInputOption: 'RAW',
-        resource: {
-            "range": range,
-            "values": [[value + previousScore, previousNumber + 1, highest]],
-        },
-        auth: auth,
-    };
-    try {
-        (sheets.spreadsheets.values.update(request)).data;
-        message.channel.send(`Score of ${value} successfully added to ${message.mentions.users.first()}.`);
-    } catch (err) {
-        message.channel.send('Failed to add score.');
-        console.log(`${err}`);
-    }
-}
-
-
 function getOfflineMembers(subjects, message) {
     const membersToReturn = [];
     if (!subjects.length) {
