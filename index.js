9731a8b77352af1d0cfef6393b1fb586b3b261cb
SiegeToaster <81945479+SiegeToaster@users.noreply.github.com>
2021-06-30 00:40:46 +0200

    Added you're* event handler and ?forgot command
    REGEX is cringe.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 /* eslint-disable no-case-declarations */
 // =====SETUP=====\\
 const Discord = require('discord.js');
@@ -30,6 +31,14 @@ let reactedUsers = [`
 > ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀
 > ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠺⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁
 `];
+const correctionGifs = [
+    'https://images-ext-2.discordapp.net/external/x_dsYvJXnDZ-He2xSj1d2UjqFzouYJLzn6qk_mff4fg/https/media.discordapp.net/attachments/728319830108667989/858313721507348480/image0.gif',
+    'https://tenor.com/view/bbc-two-bbc-two-peaky-blinders-gif-20722169',
+    'https://tenor.com/view/your-youre-gif-19011295',
+    'https://tenor.com/view/youre-your-spies-spies-in-disguise-youre-spies-gif-20405819',
+    'https://tenor.com/view/youre-gif-18693005',
+];
+let lastCorrectionIndex = 0;
 let pingVar = 'false';
 let prideVar = 'false';
 let prideFlag = '';
@@ -82,6 +91,14 @@ client.on ('message', message => {
     if (message.content === 'https://media.discordapp.net/attachments/761347053983891499/842548851310985236/delete.jpg') {
         message.channel.bulkDelete(2);
         console.log('Delete That!! activated.');
+    }
+    if (message.content.match(/(\*$)|(\*)+(you're)/gm)) {
+        let correctionIndex = Math.floor(Math.random() * correctionGifs.length);
+        while (correctionIndex == lastCorrectionIndex) {
+            correctionIndex = Math.floor(Math.random() * correctionGifs.length);
+        }
+        lastCorrectionIndex = correctionIndex;
+        message.channel.send(correctionGifs[correctionIndex] + prideFlag);
     }
     if (susVar === 'true' && message.content.toLowerCase().includes('sus')) {
         message.channel.send(`https://www.youtube.com/watch?v=0bZ0hkiIKt0 ${prideFlag}`);
@@ -148,6 +165,13 @@ client.on ('message', message => {
     case 'joe':
         message.channel.send(`joe mama ${prideFlag}`);
         break;
+
+    case 'forgot':
+        // eslint-disable-next-line no-unused-vars
+        message.channel.send(`<:FeelsNotSureMan:841539607664918558> ${prideFlag}`).then(_nil => {
+            message.channel.send(`I forgot ${prideFlag}`);
+        });
+    break;
 
 	case 'absent':
 		let offlineMembers = getOfflineMembers(message.mentions.members.array(), message);
